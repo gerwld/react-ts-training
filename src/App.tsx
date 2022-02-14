295d718d123712401e210eded6756c2e63890d0d
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-02-14 20:59:53 +0100

    Create Fc with ts interface, jeneric & enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card, { CardVariant } from './components/Card';
 
 function App() {
   return (
     <div>
-      qwe
+      <Card onClick={(num) => console.log(num, 'click')} variant={CardVariant.primary} height='200px' width='200px'>
+      <button>Кнопка</button>
+      </Card>
     </div>
   )
 }
