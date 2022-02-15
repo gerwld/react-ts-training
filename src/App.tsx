7b87fea8940fbd04722d4903bced6c15681f3073
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-02-15 11:27:54 +0100

    Create reusable Fc map List with interface & universal generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
+import axios from 'axios';
+import List from './components/List';
+import UserItem from './components/UserItem';
 
 function App() {
+  const [users, setUsers] = useState<IUser[]>([
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+      },
+    },
+    {
+      "id": 2,
+      "name": "Antonia Mueller",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+      },
+    },
+    {
+      "id": 3,
+      "name": "Lyle Leannon",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+      },
+    },
+
+  ]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+      setUsers(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
   return (
     <div>
       <Card onClick={(num) => console.log(num, 'click')} variant={CardVariant.primary} height='200px' width='200px'>
-      <button>Кнопка</button>
+        <button>Кнопка</button>
       </Card>
+      <List items={users} renderItem={(user) => <UserItem user={user} key={user.id} />} />
     </div>
   )
 }
